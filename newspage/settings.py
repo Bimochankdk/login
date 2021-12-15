abefb6f738aca4ef71cf8a39179520c36cce24f1
Bimochankdk <bimochankhadka8@gmail.com>
2021-12-15 12:14:25 +0100

    email

--- newspage/settings.py
+++ newspage/settings.py
@@ -145,9 +145,9 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/' # new
 
 
-DEFAULT_FROM_EMAIL = 'No Replay Pay <test.dev18.test@gmail.com>'
+DEFAULT_FROM_EMAIL = 'No Replay Pay <just.checking.dude@gmail.com>'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'test.dev18.test@gmail.com'
+EMAIL_HOST_USER = 'just.checking.dude@gmail.com'
 EMAIL_HOST_PASSWORD = 'iam100good'
 EMAIL_PORT = 587
